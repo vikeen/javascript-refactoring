98aac80eef34b743044f84bdf23082e76eb3c989
John Rake <john.rake12@gmail.com>
2014-06-19 14:59:18 -0500

    Add htmlStatement for html formatter customer reports

--- src/customer.js
+++ src/customer.js
@@ -21,6 +21,20 @@ Customer.prototype.statement = function() {
     return result;
 };
 
+Customer.prototype.htmlStatement = function() {
+    var result = "<h1>Rental Record for <em>" + this.name + "</em></h1><p>\n";
+
+    for (var i = 0; i < this.rentals.length; i++) {
+        // show figures for this rental
+        result += "\t" + this.rentals[i].movie.title + "\t" + this.rentals[i].charge() + "<br/>\n";
+    }
+
+    // add footer lines
+    result += "</p><p>Amount owed is <em>" + this.totalCharge() + "</em></p>\n";
+    result += "<p>You earned <em>" + this.totalFrequentRenterPoints() + "</em> frequent renter points</p>";
+    return result;
+};
+
 Customer.prototype.totalCharge = function() {
     var result = 0;
     for (var i = 0; i < this.rentals.length; i++) {
