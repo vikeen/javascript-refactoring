c728c9ef0f9fc47d44cba57d3658cc19f844585b
John Rake <john.rake12@gmail.com>
2014-06-19 13:54:13 -0500

    Move the calculateAmount logic to the rental class and rename for clarity

--- src/customer.js
+++ src/customer.js
@@ -14,7 +14,9 @@ Customer.prototype.statement = function() {
 
     for (var i = 0; i < this.rentals.length; i++) {
         var rental = this.rentals[i],
-            amount = this.calculateAmount(rental);
+            amount = rental.charge();
+
+
 
         // add frequent renter points
         frequentRenterPoints += 1;
@@ -32,30 +34,5 @@ Customer.prototype.statement = function() {
     // add footer lines
     result += "Amount owed is " + totalAmount + "\n";
     result += "You earned " + frequentRenterPoints + " frequent renter points";
-    return result;
-};
-
-Customer.prototype.calculateAmount = function(rental) {
-    result = 0;
-
-    // determine amounts for each line
-    switch (rental.movie.priceCode) {
-        case jsRefactoring.movieTypes.REGULAR:
-            result += 2;
-            if (rental.daysRented > 2) {
-                result += (rental.daysRented - 2) * 1.5;
-            }
-            break;
-        case jsRefactoring.movieTypes.NEW_RELEASE:
-            result += rental.daysRented * 3;
-            break;
-        case jsRefactoring.movieTypes.CHILDRENS:
-            result += 1.5;
-            if (rental.daysRented > 3) {
-                result += (rental.daysRented - 3) * 1.5;
-            }
-            break;
-    }
-
     return result;
 };
--- src/rental.js
+++ src/rental.js
@@ -2,3 +2,28 @@ var Rental = function(movie, daysRented) {
     this.movie = movie;
     this.daysRented = daysRented;
 };
+
+Rental.prototype.charge = function() {
+    result = 0;
+
+    // determine amounts for each line
+    switch (this.movie.priceCode) {
+        case jsRefactoring.movieTypes.REGULAR:
+            result += 2;
+            if (this.daysRented > 2) {
+                result += (this.daysRented - 2) * 1.5;
+            }
+            break;
+        case jsRefactoring.movieTypes.NEW_RELEASE:
+            result += this.daysRented * 3;
+            break;
+        case jsRefactoring.movieTypes.CHILDRENS:
+            result += 1.5;
+            if (this.daysRented > 3) {
+                result += (this.daysRented - 3) * 1.5;
+            }
+            break;
+    }
+
+    return result;
+};
