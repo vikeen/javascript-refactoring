e507c26db9a0da2f050eeacc9a044b5391bf3383
John Rake <john.rake12@gmail.com>
2014-06-19 14:00:27 -0500

    Replace Temp with Query

--- src/customer.js
+++ src/customer.js
@@ -13,22 +13,17 @@ Customer.prototype.statement = function() {
         result = "Rental Record for " + this.name + "\n";
 
     for (var i = 0; i < this.rentals.length; i++) {
-        var rental = this.rentals[i],
-            amount = rental.charge();
-
-
-
         // add frequent renter points
         frequentRenterPoints += 1;
 
         // add bonus for a two day new release rental
-        if (rental.movie.priceCode == jsRefactoring.movieTypes.NEW_RELEASE && rental.daysRented > 1) {
+        if (this.rentals[i].movie.priceCode == jsRefactoring.movieTypes.NEW_RELEASE && this.rentals[i].daysRented > 1) {
             frequentRenterPoints += 1;
         }
 
         // show figures for this rental
-        result += "\t" + rental.movie.title + "\t" + amount + "\n";
-        totalAmount += amount;
+        result += "\t" + this.rentals[i].movie.title + "\t" + this.rentals[i].charge() + "\n";
+        totalAmount += this.rentals[i].charge();
     }
 
     // add footer lines
